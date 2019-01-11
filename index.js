40dc762b703a48f6a2d33c81b4e27643b4d532c7
adrioro <artistsvillage@me.com>
2019-01-11 16:29:39 -0500

    Third commit

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 //prompt//
 
-var name = prompt("Please enter your name.");
+/*var name = prompt("Please enter your name.");
 if (name !== "") {
-  alert("Hello: " + name);
+  console.log("Hello: " + name);
 } else {
-  alert("You need to enter your name in the field.");
-  prompt("Please try again.")
+alert("You need to enter your name in the field.");
 }
 
 //confirm//
 
-var c = confirm("Are you sure?");
+/*var c = confirm("Are you sure?");
 if (c) {
   alert("You are sure.");
 } else {
   alert("You are not sure.");
 }
+
+var num1 = 5;
+var num2 = 9;
