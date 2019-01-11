4e6e92dcda4fa80251c2f32eeb3f7ec468476990
adrioro <artistsvillage@me.com>
2019-01-11 16:04:36 -0500

    Second commit Prompt

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+//prompt//
+
+var name = prompt("Please enter your name.");
+if (name !== "") {
+  alert("Hello: " + name);
+} else {
+  alert("You need to enter your name in the field.");
+  prompt("Please try again.")
+}
+
+//confirm//
+
 var c = confirm("Are you sure?");
 if (c) {
   alert("You are sure.");
