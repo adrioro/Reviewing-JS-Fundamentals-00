0be907eb71db2d46ee5f3d75512c0a5d5693984b
adrioro <artistsvillage@me.com>
2019-01-14 10:02:52 -0500

    fourth commit typeof practice

--- index.js
+++ index.js
@@ -4,17 +4,22 @@
 if (name !== "") {
   console.log("Hello: " + name);
 } else {
-alert("You need to enter your name in the field.");
+  alert("You need to enter your name in the field.");
 }
 
 //confirm//
 
-/*var c = confirm("Are you sure?");
+var c = confirm("Are you sure?");
 if (c) {
   alert("You are sure.");
 } else {
   alert("You are not sure.");
-}
+}*/
 
 var num1 = 5;
 var num2 = 9;
+var result;
+
+if (typeof result === "undefined") {
+  result = num1 + num2;
+}
